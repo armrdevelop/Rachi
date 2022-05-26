f25c23217cd60a6e28a2d6b72c3037725f017b07
armrdevelop <armrdevelop@gmail.com>
2022-05-26 14:53:24 -0300

    section add

---
+++ src/components/beneficios/index.tsx
@@ -0,0 +1,11 @@
+export const Benefices = () => {
+  return (
+    <section className="container-fluid bg-whiter-200">
+      <div className="container flex justify-center items-center md:flex-row flex-col md:min-h-[476px] min-h-[644px]">
+        <div className="title-section">
+          <h2></h2>
+        </div>
+      </div>
+    </section>
+  )
+}
